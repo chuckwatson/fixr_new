a268b0311d8260aaee36b543fc72f1651daa2b70
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-02-22 11:49:08 +0000

    jobs controller done

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,37 @@
+class JobsController < ApplicationController
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @service = Service.find(params[:service_id])
+    @job = Job.new(job_params)
+    @job.service = @service
+    if @job.save
+      redirect_to shop_path(@shop)
+    else
+      render "shop/show"
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to shop_path(@shop)
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:name, :description, :price)
+  end
+end
