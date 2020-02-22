e27602d871a93ed8c43221822ac737c59bd39434
Csaba Krisztan <krisztan.csaba@gmail.com>
2020-02-22 11:20:48 +0000

    bookings controller second try

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,39 @@
 class BookingsController < ApplicationController
+  def new
+    @shop = Shop.find(params[:shop_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Shop.new(booking_params)
+    @shop = Shop.find(params[:shop_id])
+    @booking.shop = @shop
+    @booking.user = current_user
+    if @booking.save
+      redirect_to shop_booking_path(@shop, @booking)
+    else
+      render 'shops/show'
+    end
+  end
+
+  def index
+    @user = current_user
+    @bookings = @user.bookings
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_bookings_path(current_user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date)
+  end
 end
