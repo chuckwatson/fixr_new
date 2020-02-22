88137d83f5ed2f706480c93118349de914c685ec
Charlie Watson <charliewatson30@hotmail.com>
2020-02-22 16:46:38 +0000

    fixing seeds and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reviews
-  has_many :favourites
+  # has_many favourites
   has_many :bookings, dependent: :destroy
-  belongs_to :shop
+  has_many :shops
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: :true
+  validates :email, presence: true
 
 end
