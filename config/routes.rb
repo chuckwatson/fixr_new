2e0138b68ebae49d6e6982f7ac7b97727e059074
Charlotte Waller <charlotteawaller@gmail.com>
2020-02-22 10:30:08 +0000

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :shops do #index #show #new/create #delete #update
+    resources :bookings, only: [:new, :create, :show]
+  end
+
+  resources :shops do #index #show #new/create #delete #update
+    resources :reviews, only: [:new, :create, :destroy]
+  end
+
+  resources :shops do #index #show #new/create #delete #update
+    resources :services, only: [:new, :create, :destroy]
+  end
+
+  resources :users do
+    resources :bookings, only: [:index]
+  end
+
 end
