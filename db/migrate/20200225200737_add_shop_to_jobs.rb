7855cd054eb63078d1b8a56536ec43098fd0b0eb
Charlotte Waller <charlotteawaller@gmail.com>
2020-02-29 13:59:48 +0000

    Migration fixes

--- db/migrate/20200225200737_add_shop_to_jobs.rb
+++ db/migrate/20200225200737_add_shop_to_jobs.rb
@@ -3,10 +3,10 @@ class AddShopToJobs < ActiveRecord::Migration[5.2]
     add_column :jobs, :shop_id, :integer
   end
 
-  create_table :bookings do |t|
-      t.date :date
-      t.references :user, foreign_key: true
-      t.references :service, foreign_key: true
-      t.timestamps
-    end
+  # create_table :bookings do |t|
+  #     t.date :date
+  #     t.references :user, foreign_key: true
+  #     t.references :service, foreign_key: true
+  #     t.timestamps
+  #   end
 end
--- db/migrate/20200225200838_remove_services_from_bookings.rb
+++ db/migrate/20200225200838_remove_services_from_bookings.rb
@@ -1,5 +1,5 @@
 class RemoveServicesFromBookings < ActiveRecord::Migration[5.2]
   def change
-    remove_column :bookings, :service_id, :integer
+    # remove_column :bookings, :service_id, :integer
   end
 end
