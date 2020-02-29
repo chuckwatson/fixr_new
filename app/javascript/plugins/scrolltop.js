c58a01582024e3a6bf302022e12101c49267f874
Csaba Krisztan <krisztan.csaba@gmail.com>
2020-02-29 12:38:44 +0000

    unhighlight and scroll up function added

--- app/javascript/plugins/scrolltop.js
+++ app/javascript/plugins/scrolltop.js
@@ -1,5 +1,27 @@
-window.scroll({
- top: 0,
- left: 0,
- behavior: 'smooth'
+// footer.addEventListener('click', (event) => {
+//   card.classList.toggle('highlight-card');
+//   window.scroll({
+//    top: 0,
+//    left: 0,
+//    behavior: 'smooth'
+//   });
+// }
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#footer').fadeIn();
+    } else {
+        $('#footer').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#footer").click(function(event) {
+        event.preventDefault();
+        document.querySelectorAll('.highlight-card').forEach((element) => {
+          element.classList.remove('highlight-card')
+        });
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
 });
