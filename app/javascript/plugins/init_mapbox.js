06f10c56e49bd40eca10ebcd87763db924b41488
Csaba Krisztan <krisztan.csaba@gmail.com>
2020-02-29 16:08:13 +0000

    fixing javascript trying to add search bar

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -41,6 +41,8 @@ const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 25, maxZoom: 15 });
+  // map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+  //                                     mapboxgl: mapboxgl }));
 };
 
 
