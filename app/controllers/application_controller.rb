54be5b4aa9039fc86bfda5f2ba5fefb1056bf03d
Charlie Watson <charliewatson30@hotmail.com>
2020-03-03 22:22:38 +0000

    favorites button update to red

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def favorite_text
-    return @favorite_exists ? "far fa-heart" : "fas fa-heart"
+    return @favorite_exists ? "fas fa-heart" : "far fa-heart"
   end
 
   helper_method :favorite_text
