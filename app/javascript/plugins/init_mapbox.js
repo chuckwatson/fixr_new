cefb27864d075b064447ecdb102fb424d6aab0e1
Csaba Krisztan <krisztan.csaba@gmail.com>
2020-03-07 12:55:24 +0000

    index page finished

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -40,7 +40,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 25, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 35, maxZoom: 15 });
   // map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
   //                                     mapboxgl: mapboxgl }));
 };
@@ -63,39 +63,6 @@ const initMapbox = () => {
 };
 
 
-// function geoFindMe() {
-
-//   const status = document.querySelector('#status');
-//   // const mapLink = document.querySelector('#map-link');
-
-//   // mapLink.href = '';
-//   // mapLink.textContent = '';
-
-//   function success(position) {
-//     const latitude  = position.coords.latitude;
-//     const longitude = position.coords.longitude;
-//     console.log(latitude)
-//     console.log(longitude)
-//     status.textContent = '';
-//     return [latitude, longitude]
-
-//     // mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-
-//     // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-//   }
-
-//   function error() {
-//     status.textContent = 'Unable to retrieve your location';
-//   }
-
-//   if (!navigator.geolocation) {
-//     status.textContent = 'Geolocation is not supported by your browser';
-//   } else {
-//     status.textContent = 'Locating…';
-//     navigator.geolocation.getCurrentPosition(success, error);
-//   }
-
-// }
 
 document.querySelector('#find-me').addEventListener('click', (event) => {
   const status = document.querySelector('#status');
@@ -143,3 +110,37 @@ document.querySelector('#find-me').addEventListener('click', (event) => {
 
 export { initMapbox };
 
+
+// function geoFindMe() {
+
+//   const status = document.querySelector('#status');
+//   // const mapLink = document.querySelector('#map-link');
+
+//   // mapLink.href = '';
+//   // mapLink.textContent = '';
+
+//   function success(position) {
+//     const latitude  = position.coords.latitude;
+//     const longitude = position.coords.longitude;
+//     console.log(latitude)
+//     console.log(longitude)
+//     status.textContent = '';
+//     return [latitude, longitude]
+
+//     // mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+
+//     // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+//   }
+
+//   function error() {
+//     status.textContent = 'Unable to retrieve your location';
+//   }
+
+//   if (!navigator.geolocation) {
+//     status.textContent = 'Geolocation is not supported by your browser';
+//   } else {
+//     status.textContent = 'Locating…';
+//     navigator.geolocation.getCurrentPosition(success, error);
+//   }
+
+// }
