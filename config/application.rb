45ec230c42cc6f6a693c62f45de9e9f83b4c9b9b
Charlie Watson <charliewatson30@hotmail.com>
2020-03-10 19:23:03 +0000

    pushing to heroku

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Fixr
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.web_console.whitelisted_ips = '80.4.87.163'
+    # config.web_console.whitelisted_ips = '80.4.87.163'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
