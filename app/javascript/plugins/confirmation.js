12f9b769f7ac283b5501dd314cbd3986c16e0588
Charlotte Waller <charlotteawaller@gmail.com>
2020-03-14 15:39:00 +0000

    Typed.js only loading on required page

--- app/javascript/plugins/confirmation.js
+++ app/javascript/plugins/confirmation.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Connecting Cyclists", "With Independent Stores"],
-    typeSpeed: 50,
-    loop: true
-  });
+  const element = document.getElementById('banner-typed-text');
+  if (element) {
+    new Typed('#banner-typed-text', {
+      strings: ["Connecting Cyclists", "With Independent Stores"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
