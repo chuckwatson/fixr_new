c7a85b5288d7a496ee4bc3830029bec304d7a57b
Charlie Watson <charliewatson30@hotmail.com>
2020-03-14 13:27:44 +0000

    fixing service list

--- app/javascript/plugins/scrollbottom.js
+++ app/javascript/plugins/scrollbottom.js
@@ -1,34 +1,3 @@
-// footer.addEventListener('click', (event) => {
-//   card.classList.toggle('highlight-card');
-//   window.scroll({
-//    top: 0,
-//    left: 0,
-//    behavior: 'smooth'
-//   });
-// }
-// $(window).scroll(function() {
-//     var height = $(window).scrollBottom();
-//     if (height > 100) {
-//         $('#show_footer').fadeIn();
-//     } else {
-//         $('#show_footer').fadeOut();
-//     }
-// });
-// $(document).ready(function() {
-//     $("#show_footer").click(function(event) {
-//         event.preventDefault();
-
-
-//         document.querySelectorAll('#reviews').forEach((element) => {
-//           element.classList.remove('reviews')
-//         });
-
-
-//         $("html, body").animate({ scrollBottom: 0 }, "slow");
-//         return false;
-//     });
-
-// // });
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelector('#show_review').addEventListener('click', (event) => {
     document.querySelector('.review-container').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
