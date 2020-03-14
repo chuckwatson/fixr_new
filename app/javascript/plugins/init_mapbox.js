f382c5316d08d1e1fe3591b953591a77c8e442b9
Csaba Krisztan <krisztan.csaba@gmail.com>
2020-03-14 11:09:24 +0000

    changing zoom

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const buildMap = () => {
   return new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/kcsaba/ck6xiz8zm0jms1is210ohjkx8',
-     maxZoom: 15, duration: 0
+      minzoom:1 , maxZoom: 15, padding:35
   });
 };
 
@@ -41,7 +41,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 35, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 35, maxZoom: 15});
   // map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
   //                                     mapboxgl: mapboxgl }));
 };
